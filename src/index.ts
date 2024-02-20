8b3777159220ecc651dc6bd7a37e840c8251bd68
hoangnq <nguyen.quy.hoang@sun-asterisk.com>
2024-02-20 13:34:14 +0700

    home page

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,15 @@ import cookieParser from 'cookie-parser';
 import router from './routes';
 import * as dotenv from 'dotenv';
 import logger from 'morgan';
+import i18next from 'i18next';
+import i18nextMiddleware from 'i18next-http-middleware';
+import i18nextBackend from "i18next-fs-backend"
+
 
 dotenv.config();
-
-import { AppDataSource } from './config/database';
-
 const app = express();
 const port = 3000;
-
-console.log(process.env);
-
+import { AppDataSource } from './config/database';
 AppDataSource.initialize()
   .then(() => {
     console.log('Data Source has been initialized!');
@@ -26,7 +25,32 @@ AppDataSource.initialize()
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+i18next
+  .use(i18nextBackend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    fallbackLng: 'vi',
+    preload: ['vi', 'en'],
+    supportedLngs: ['vi', 'en'],
+    saveMissing: true,
+    backend: {
+      loadPath: path.join(__dirname, 'locales/{{lng}}/{{ns}}.json'),
+      addPath: path.join(__dirname, 'locales/{{lng}}/{{ns}}.missing.json'),
+    },
+    detection: {
+      order: ['querystring', 'cookie'],
+      caches: ['cookie'],
+      lookupQuerystring: 'locale', //query string on url (?locale=en/vi)
+      lookupCookie: 'locale',
+      ignoreCase: true,
+      cookieSecure: false,
+    }
+  });
+
+app.use(i18nextMiddleware.handle(i18next));
+
 // Set view engine
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'pug');
 
 // Parse URL-encoded and JSON bodies
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,22 @@
 import express from 'express';
-import userRoute from './user';
-
+import authorRoute from './author.route';
+import booGenreRoute from './bookGenre.route';
+import bookInstanceRoute from './bookInstance.route';
+import bookRoute from './book.route';
+import genreRoute from './genre.route';
+import { Request, Response, NextFunction } from 'express';
+import { index } from '../controllers/book.controller'
 const router = express.Router();
 
-router.use('/user', userRoute);
+router.get('/', index)
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.render('index');
+})
+
+router.use('/author', authorRoute);
+router.use('/book', bookRoute);
+router.use('/bookGenre', booGenreRoute);
+router.use('/bookInstance', bookInstanceRoute);
+router.use('/genre', genreRoute);
 
 export default router;
